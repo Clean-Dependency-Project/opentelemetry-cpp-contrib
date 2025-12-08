68abe4d284e9ffa0e22dd0cb49878fbbbd35002e
marpbri09 <132700882+Brihadh-Marpadga@users.noreply.github.com>
2025-12-08 16:31:37 +0000

    fxed dockerfile  with python

--- instrumentation/otel-webserver-module/scripts/gyp/r1919/gyp_main.py
+++ instrumentation/otel-webserver-module/scripts/gyp/r1919/gyp_main.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
 
+#!/usr/bin/env python3
 # Copyright (c) 2009 Google Inc. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# BSD-style license; see LICENSE
 
 import sys
 
-# TODO(mark): sys.path manipulation is some temporary testing stuff.
 try:
-  import gyp
-except ImportError, e:
-  import os.path
-  sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), 'pylib'))
-  import gyp
+    # gyp-next exposes a 'gyp' module with the same entrypoint name
+    import gyp
+except ImportError as e:
+    # Fallback to local 'pylib' only if you know it's Python 3 compatible.
+    import os.path
+    sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), 'pylib'))
+    import gyp
 
 if __name__ == '__main__':
-  sys.exit(gyp.script_main())
+    sys.exit(gyp.script_main())
